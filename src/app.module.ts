c4662161f4e01e7c6b4a23b0cd6830bcf5447f7c
zhangbl <zhangbl@guotailimin.com>
2020-09-17 16:46:57 +0800

    chore: update config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+const envFilePath = [`.env.${process.env.BIZ_ENV}`, '.env'];
+if (process.env.BIZ_ENV === 'local') envFilePath.unshift('.env.local');
+
+ConfigModule.forRoot({ envFilePath });
 
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
